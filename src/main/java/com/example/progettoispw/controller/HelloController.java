cfe0e10e20c36a4f00f4fcbd0848d676f8d40c5d
MatteoPaladini <paladinimatteo599@gmail.com>
2026-03-20 11:56:00 +0100

    Risoluzione Issues SonarCloud.

--- src/main/java/com/example/progettoispw/controller/HelloController.java
+++
@@ -1,4 +0,0 @@
-package com.example.progettoispw.controller;
-
-public class HelloController {
-}
---
+++ src/main/java/com/example/progettoispw/controller/package-info.java
@@ -0,0 +1,7 @@
+/*
+Questo package contiene tutte le classi relative ai controller:
+-   grafici, usati dalle pagine fxml in genere per mostrare o prendere dati
+-   logici, implementano la logica ricevendo i dati forniti dai controller grafici
+-   cli, usati per sviluppare l'intera logica della CLI
+ */
+package com.example.progettoispw.controller;
