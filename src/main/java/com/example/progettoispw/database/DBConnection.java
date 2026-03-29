7c6ea16a19af0562e6d451e70d1a2ef5f5571b06
MatteoPaladini <paladinimatteo599@gmail.com>
2026-03-29 18:55:24 +0200

    Risoluzione conflitti view-logic.

--- src/main/java/com/example/progettoispw/database/DBConnection.java
+++ src/main/java/com/example/progettoispw/database/DBConnection.java
@@ -33,9 +33,9 @@ public class DBConnection {
     public static DBConnection getInstance(){
         if(instance == null){
             synchronized (DBConnection.class){
-                if(instance == null){
+
                     instance = new DBConnection();
-                }
+
             }
 
         }
